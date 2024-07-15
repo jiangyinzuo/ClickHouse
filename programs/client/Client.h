b2320f1025b144d4e6bc15e615a9dc9c76dd336b
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2024-07-15 16:36:45 +0000

    Refactoring

--- programs/client/Client.h
+++ programs/client/Client.h
@@ -1,14 +1,16 @@
 #pragma once
 
-#include <Client/ClientBase.h>
+#include <Client/ClientApplicationBase.h>
 
 
 namespace DB
 {
 
-class Client : public ClientBase
+class Client : public ClientApplicationBase
 {
 public:
+    using Arguments = ClientApplicationBase::Arguments;
+
     Client()
     {
         fuzzer = QueryFuzzer(randomSeed(), &std::cout, &std::cerr);
